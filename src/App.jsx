8a97680c0fe01dffe8e85cd17311de4c2fab8791
MojoisMe <2377862945@qq.com>
2024-07-03 10:42:56 +0800

    add checkLogin Function

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import HomePage from './pages/Home/Home'
-import HeaderLayout from './layout/HeaderLayout';
+import Header from './components/Header/Header';
 import LoginAndRegister from './pages/Login/index';
 import FindInterestsPage from './pages/FindInterests/index';
 import MyInterests from './pages/MyInterests/index';
-
+let curr_user = {
+  id: '123',
+  name: 'Mojo',
+  email: 'mojo@example.com',
+  imageUrl: '/logo.svg',
+}
 export default function App() {
+  function checkLogin(page){
+    if(curr_user.id){
+      return page;
+    }else{
+      return <Navigate to="/login" />;
+    }
+  }
   return (
     <>
     <Router>
-      <HeaderLayout/>
+      <Header user={curr_user}/>
       <Routes>
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/login" element={<LoginAndRegister />} />
-        <Route path="/findInterests" element={<FindInterestsPage />} />
-        <Route path="/myInterests" element={<MyInterests />} />
+        <Route path='/' element={<Navigate to="/home" />}/>
+        <Route path="/login" element={curr_user.id ? <Navigate to="/home"/> : <LoginAndRegister />} />
+        <Route path="/home" element={checkLogin(<HomePage />)} />
+        <Route path="/findInterests" element={checkLogin(<FindInterestsPage />)} />
+        <Route path="/myInterests" element={checkLogin(<MyInterests />)} />
       </Routes>
     </Router>
     </>
